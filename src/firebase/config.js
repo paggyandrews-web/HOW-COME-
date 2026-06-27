ea536a365749d0a840c038808fda9226abb5299d
DREWS <paggyandrews@gmail.com>
2026-06-27 19:45:44 +0530

    fix: add Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,12 +8,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDUVOg7Gjr7wHGD6dhYmaovUgLwcMTbvGY",
+  authDomain: "how-come-80e31.firebaseapp.com",
+  projectId: "how-come-80e31",
+  storageBucket: "how-come-80e31.firebasestorage.app",
+  messagingSenderId: "651167370939",
+  appId: "1:651167370939:web:7ec4ffd4e9949119a5639e",
+  measurementId: "G-03TYG2BCG9"
 }
 
 const app = initializeApp(firebaseConfig)
